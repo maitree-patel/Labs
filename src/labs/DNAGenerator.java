12bcb20003cd50fab7ee55267a17f14bf94fe9cf
maitreepatel <maitreepatel@10.97.234.242>
2024-08-28 15:04:20 -0400

    first commit

---
+++ src/labs/DNAGenerator.java
@@ -0,0 +1,15 @@
+package labs;
+
+import java.util.Random;
+
+public class DNAGenerator {
+	public static void main(String[] args) {
+		Random random = new Random();
+		
+		System.out.println(random.nextInt(4));
+		System.out.println(random.nextInt(4));
+		
+		
+		
+	}
+}
