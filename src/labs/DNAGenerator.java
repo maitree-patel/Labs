f752eaf12c8ce75069941c4878a9b00691756949
maitreepatel <maitreepatel@10.106.13.191>
2024-08-28 18:36:25 -0400

    in progress 4...

--- src/labs/DNAGenerator.java
+++ src/labs/DNAGenerator.java
@@ -6,11 +6,11 @@ public class DNAGenerator {
 		
 		String seq = "";
 		int x = 0;
-		int a_total = 0;
-		int c_total = 0;
-		int g_total = 0;
-		int t_total = 0;
-		
+		float a_total = 0;
+		float c_total = 0;
+		float g_total = 0;
+		float t_total = 0;
+				
 		while (x<1000)
 		{
 			for (int y=0; y<3; y++) {
@@ -34,8 +34,13 @@ public class DNAGenerator {
 			}
 			x++;
 		}
+		
+		float a_proportion = (a_total/3000)*100;
+		float c_proportion = (c_total/3000)*100;
+		float g_proportion = (g_total/3000)*100;
+		float t_proportion = (t_total/3000)*100;
+		
 		System.out.println(seq);
-		System.out.println("test");
 	}
 }
 
