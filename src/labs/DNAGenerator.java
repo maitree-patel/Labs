abc3d939f5f73bfef0fc731afb10d77bc112a671
Maitree Patel <134908239+maitree-patel@users.noreply.github.com>
2024-08-28 16:15:18 -0400

    Update DNAGenerator.java

--- src/labs/DNAGenerator.java
+++ src/labs/DNAGenerator.java
@@ -5,11 +5,18 @@ import java.util.Random;
 public class DNAGenerator {
 	public static void main(String[] args) {
 		Random random = new Random();
-		
-		System.out.println(random.nextInt(4));
-		System.out.println(random.nextInt(4));
-		
-		
+		String seq = "";
+		int x = 0;
+		while (x<2)
+		{
+			for (int y=0; y<3; y++) {
+				int nucleotide = random.nextInt(4);
+				if (nucleotide == 0) {
+					seq = seq + "A";
+				}
+			}
+			x++;
+		}
 		
 	}
 }
