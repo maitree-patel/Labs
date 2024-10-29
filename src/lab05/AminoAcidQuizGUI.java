92b28c7cb659cf4c730d5bbca49f6b909dd69783
maitreepatel <maitreepatel@maitrees-air.lan>
2024-10-28 21:05:37 -0400

    GUI window test

---
+++ src/lab05/AminoAcidQuizGUI.java
@@ -0,0 +1,44 @@
+package lab05;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JTextField;
+
+public class AminoAcidQuizGUI extends JFrame
+{
+	private JButton doubleButton = new JButton("Start Quiz!");
+	private JTextField aTextField = new JTextField();
+	
+	public AminoAcidQuizGUI() 
+	{
+		super("Amino Acid Quiz");
+		
+		//setting size of window
+		setSize(200,200);
+		
+		//location of window in the center
+		setLocationRelativeTo(null);
+		
+		//adding button and text
+		getContentPane().setLayout(new BorderLayout());
+		getContentPane().add(doubleButton, BorderLayout.SOUTH);
+		getContentPane().add(aTextField, BorderLayout.CENTER);
+		
+		//displays text on GUI window
+		aTextField.setText("GUI test!");
+		
+		//exit java on closing window
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		//making window visible
+		setVisible(true);
+	}
+	
+	public static void main(String[] args)
+	{
+		new AminoAcidQuiz();
+	}
+
+}
