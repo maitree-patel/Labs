2e4b83ca27fa08802a60e47dc3514158795373eb
maitreepatel <maitreepatel@10.131.214.104>
2024-11-04 16:25:42 -0500

    Final Draft 1.0 (timer not stopping on cancel)

--- src/lab05/AminoAcidQuizGUI.java
+++ src/lab05/AminoAcidQuizGUI.java
@@ -1,4 +1,3 @@
-//
 package lab05;
 
 import javax.swing.*;
@@ -6,124 +5,165 @@ import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
+import javax.swing.Timer;
 
-public class AminoAcidQuizGUI extends JFrame implements ActionListener {
-	
-	@Override
-    public void actionPerformed(ActionEvent e) {
-        // check the answer when submit button is clicked
-        checkAnswer();
-    }
 
-    private JTextField answerField; //an answer field
-    private JLabel questionLabel, scoreLabel, timerLabel;
-    private JButton submitButton;
-    private Timer quizTimer;
+public class AminoAcidQuizGUI {
+    private JFrame frame;
+    private JTextField questionField, answerField;
+    private JButton startButton, submitButton, cancelButton;
+    private JLabel timerLabel, scoreLabel;
+    private Timer timer;
+    private int timeLeft = 30;
+    private int score = 0;
+    
+    char aa_code[] = {'A', 'R', 'N', 'D', 
+	          		  'C', 'Q', 'E', 'G', 
+	          		  'H', 'I', 'L', 'K', 
+	          		  'M', 'F', 'P', 'S', 
+	          		  'T', 'W', 'Y', 'V'};
 
-    // initializing quiz data
-    private int totalDuration = 30;
-    private int timeRemaining;
-    private int totalQuestions = 0;
-    private int correctAnswers = 0;
-    private Random random = new Random();
-
-    private char[] aaCodes = {'A', 'R', 'N', 'D', 'C', 'Q', 'E', 'G', 'H', 'I', 'L', 'K', 'M', 'F', 'P', 'S', 'T', 'W', 'Y', 'V'};
-    private String[] aaNames = {"alanine", "arginine", "asparagine", "aspartic acid", "cysteine", "glutamine", "glutamic acid", 
-                                "glycine", "histidine", "isoleucine", "leucine", "lysine", "methionine", "phenylalanine", 
-                                "proline", "serine", "threonine", "tryptophan", "tyrosine", "valine"};
-    private int currentIndex;
+    String aa_fullname[] = {"alanine", "arginine", "asparagine", "aspartic acid", 
+	                		"cysteine", "glutamine", "glutamic acid", "glycine", 
+	                		"histidine", "isoleucine", "leucine", "lysine", 
+	                		"methionine", "phenylalanine", "proline", "serine", 
+	                		"threonine", "tryptophan", "tyrosine", "valine"};
+    
+    String aa_ans;
+    String aa_ques;
+    int totalQues;
 
     public AminoAcidQuizGUI() {
-        // Set up the main window
-        setTitle("Amino Acid Quiz");
-        setSize(400, 400);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new BorderLayout());
+        frame = new JFrame("Amino Acid Quiz");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 300);
+        frame.setLayout(new BorderLayout());
 
-        // quiz panel for questions, answers, and score
-        JPanel quizPanel = new JPanel();
-        quizPanel.setLayout(new GridLayout(4, 1));
-        
-        questionLabel = new JLabel("Press 'Start Quiz' to begin!");
-        quizPanel.add(questionLabel);
-        
-        answerField = new JTextField(5);
-        quizPanel.add(answerField);
-        
+        // Main panel for quiz content
+        JPanel mainPanel = new JPanel();
+        mainPanel.setLayout(new BorderLayout());
+
+        questionField = new JTextField();
+        questionField.setEditable(false);
+        mainPanel.add(questionField, BorderLayout.NORTH);
+
+        timerLabel = new JLabel("Time left: 30 seconds", SwingConstants.CENTER);
+        mainPanel.add(timerLabel, BorderLayout.CENTER);
+
+        frame.add(mainPanel, BorderLayout.NORTH);
+
+        // input answer panel
+        JPanel answerPanel = new JPanel(new GridLayout(2, 1));
+
+        JPanel inputPanel = new JPanel();
+        answerField = new JTextField(10);
+        inputPanel.add(new JLabel("Answer:"));
+        inputPanel.add(answerField);
+        answerPanel.add(inputPanel);
+
+        // defining buttons
+        JPanel buttonPanel = new JPanel();
+        startButton = new JButton("Start Quiz");
+        startButton.addActionListener(new StartButtonListener());
+        buttonPanel.add(startButton);
+
         submitButton = new JButton("Submit Answer");
-        submitButton.addActionListener(this);
-        quizPanel.add(submitButton);
-        
-        scoreLabel = new JLabel("Score: 0/0");
-        quizPanel.add(scoreLabel);
+        submitButton.addActionListener(new SubmitButtonListener());
+        buttonPanel.add(submitButton);
 
-        // timer display
-        timerLabel = new JLabel("Time Remaining: " + totalDuration + " seconds");
-        add(quizPanel, BorderLayout.CENTER);
-        add(timerLabel, BorderLayout.SOUTH);
+        cancelButton = new JButton("Cancel Quiz");
+        cancelButton.addActionListener(new CancelButtonListener());
+        buttonPanel.add(cancelButton);
 
-        // timer setup
-        quizTimer = new Timer(1000, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                timeRemaining--;
-                timerLabel.setText("Time Remaining: " + timeRemaining + " seconds");
-                if (timeRemaining <= 0) {
-                    quizTimer.stop();
-                    endQuiz();
-                }
-            }
-        });
+        answerPanel.add(buttonPanel);
+        frame.add(answerPanel, BorderLayout.CENTER);
 
-        answerField.setEnabled(false);
-        submitButton.setEnabled(false);
+        // score display
+        scoreLabel = new JLabel("Score: 0", SwingConstants.CENTER);
+        frame.add(scoreLabel, BorderLayout.SOUTH);
+
+        frame.setVisible(true);
     }
 
-    //method to start quiz 
     private void startQuiz() {
-        timeRemaining = totalDuration;
-        totalQuestions = 0;
-        correctAnswers = 0;
-        quizTimer.start();
-
-        answerField.setEnabled(true);
-        submitButton.setEnabled(true);
-        questionLabel.setText("What is the single character code for...");
-        askQuestion(); // asking a question when quiz starts
+        timeLeft = 30;
+        score = 0;
+        scoreLabel.setText("Score: 0");
+        timerLabel.setText("Time left: 30 seconds");
+        generateRandomQuestion();
+        startTimer();
     }
 
-    private void askQuestion() {
-        currentIndex = random.nextInt(aaNames.length);
-        questionLabel.setText("What is the single character code for " + aaNames[currentIndex] + "?");
-        answerField.setText("");
+    private void generateRandomQuestion() {
+        Random random = new Random();
+        /*int index = random.nextInt(aminoAcids.length);
+        currentQuestion = aminoAcids[index][0];
+        currentAnswer = aminoAcids[index][1];
+        questionField.setText("What is the single-letter code for " + currentQuestion + "?");
+        */
+        int index = random.nextInt(20);
+		
+		aa_ques = aa_fullname[index];
+		aa_ans = ""+aa_code[index]; 
+		totalQues++;
+		questionField.setText("What is the single character code for " + aa_ques + " ?");
     }
 
-    private void checkAnswer() {
-        String answer = answerField.getText().toUpperCase();
-        totalQuestions++;
-
-        if (answer.equals(String.valueOf(aaCodes[currentIndex]))) {
-            correctAnswers++;
-            JOptionPane.showMessageDialog(this, "Correct! Good job!");
-        } else {
-            JOptionPane.showMessageDialog(this, "Incorrect. The correct answer is " + aaCodes[currentIndex] + ".");
+    private void startTimer() {
+       Timer timer = new Timer(1000, new ActionListener()
+       {
+    	    @Override
+            public void actionPerformed(ActionEvent e) 
+    	    {
+                if (timeLeft > 0) {
+                    timeLeft--;
+                    timerLabel.setText("Time left: " + timeLeft + " seconds");
+                } else {
+                    ((Timer) e.getSource()).stop();
+                    timerLabel.setText("Time's up!");
+            }
         }
-
-        scoreLabel.setText("Score: " + correctAnswers + "/" + totalQuestions);
-        askQuestion();
+       });
+        timer.start();
     }
 
-    private void endQuiz() {
-        answerField.setEnabled(false);
-        submitButton.setEnabled(false);
-        JOptionPane.showMessageDialog(this, "Time's up! Your final score is " + correctAnswers + "/" + totalQuestions);
+    private class StartButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            startQuiz();
+            answerField.setEditable(true);
+        }
+    }
+
+    private class SubmitButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (timeLeft > 0) {
+                String answer = answerField.getText().trim().toUpperCase();
+                if (answer.equals(aa_ans)) {
+                    score++;
+                    scoreLabel.setText("Score: " + score + "/" + totalQues);
+                }
+                answerField.setText("");
+                generateRandomQuestion();
+            }
+        }
+    }
+
+    private class CancelButtonListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            if (timer != null && timer.isRunning()) {
+                ((Timer) e.getSource()).stop();
+            }
+            questionField.setText("");
+            timerLabel.setText("Quiz Over!");
+            scoreLabel.setText("Your Total Score is: " + score + "/" + totalQues);
+            answerField.setEditable(false);
+        }
     }
 
     public static void main(String[] args) {
-        SwingUtilities.invokeLater(() -> {
-            AminoAcidQuizGUI quizApp = new AminoAcidQuizGUI();
-            quizApp.setVisible(true);
-            quizApp.startQuiz();
-        });
+        new AminoAcidQuizGUI();
     }
 }
